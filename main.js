b0b74e36f516adaf2e9c6505453544b9768090c8
tuananhhust05 <tuananhhust05@gmail.com>
2022-07-20 08:27:47 +0700

    update

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const { autoUpdater } = require('electron-updater') // import autoupdate
-
-const { app, BrowserWindow } = require("electron");
+const { app,dialog, BrowserWindow } = require("electron");
 const path = require('path');
 
 app.whenReady().then(createWindow); // app readey thì tạo ra cửa sổ 
@@ -32,3 +31,27 @@ app.on('activate',()=>{
     createWindow()
   }
 })
+autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
+	const dialogOpts = {
+		type: 'info',
+		buttons: ['Ok'],
+		title: 'Application Update',
+		message: process.platform === 'win32' ? releaseNotes : releaseName,
+		detail: 'A new version is being downloaded.'
+	}
+	dialog.showMessageBox(dialogOpts, (response) => {
+
+	});
+})
+autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
+	const dialogOpts = {
+		type: 'info',
+		buttons: ['Restart', 'Later'],
+		title: 'Application Update',
+		message: process.platform === 'win32' ? releaseNotes : releaseName,
+		detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+	};
+	dialog.showMessageBox(dialogOpts).then((returnValue) => {
+		if (returnValue.response === 0) autoUpdater.quitAndInstall()
+	})
+});
